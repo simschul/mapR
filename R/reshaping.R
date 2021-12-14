e7b75fb59a581f654483196f769c51bdb1d37cec
Simon Schulte <s.pedro@gmx.de>
2021-12-14 15:55:27 +0100

    added tests and function descriptions

--- R/reshaping.R
+++ R/reshaping.R
@@ -1,12 +1,14 @@
-#' Title
+#' Reshapes a correspondence table from long to wide format.
+#' Wrapper around `data.table::dcast`
 #'
-#' @param x
-#' @param source
-#' @param target
-#' @param value
+#' @param x a correspondence table in long format. Must have at least 3 columns: source and target classification, and mapping infromation (numeric/boolean)
+#' @param source location (numeric) or name (character) of column of source classification
+#' @param target location (numeric) or name (character) of column of target classification
+#' @param value location (numeric) or name (character) of value column
 #' @param return
 #'
 #' @return
+#' @import data.table
 #' @export
 #'
 #' @examples
@@ -20,26 +22,27 @@ CT_to_wide <- function(x,
   if (is.numeric(target)) target <- colnames(x)[target]
   if (is.numeric(value)) value <- colnames(x)[value]
 
-  x_wide <- dcast(x, get(source) ~ get(target),
+  x_wide <- dcast(x,
+                  paste0(source, ' ~ ', target), # formular as character string, see: https://stackoverflow.com/questions/31295376/in-r-dcast-in-function-pass-column-names-again
                   value.var = value,
                   fill = 0)
-
-  setnames(x_wide, 'source', source)
-
+  setkey(x_wide, NULL)
   if (return == 'matrix') x_wide <- as.matrix(x_wide, rownames = 1)
 
   return(x_wide)
 }
 
 
-#' Title
+#' Reshapes correspondence table from wide to long format.
+#' Wrapper around `data.table::melt`
 #'
-#' @param x
-#' @param source_name
-#' @param target_name
-#' @param sort_by
+#' @param x a CT in wide format. Either matrix with rownames (source classification) and column names (target classfication), or data.table with the source classifaction as the first column
+#' @param source_name name of the source classication. Defaults to 'source' (if x is matrix), or the name of the first column (if x is data.table)
+#' @param target_name name of the target classifaction. Defaults to 'target'
+#' @param sort_by either 'source' (default) or 'target'
 #'
-#' @return
+#' @return a CT in long format (=data.table with 3 columns: source and target classification, and mapping infromation (numeric/boolean))
+#' @import data.table
 #' @export
 #'
 #' @examples
@@ -47,14 +50,14 @@ CT_to_wide <- function(x,
 CT_to_long <- function(x, source_name = 'source',
                        target_name = 'target',
                        sort_by = 'source') {
-  if (is.matrix(x)) x <- as.data.table(x, keep.rownames = source_name)
+  if (is.matrix(x)) x <- data.table::as.data.table(x, keep.rownames = source_name)
   source_name <- colnames(x)[1]
-  x <- melt(x, id.var = source_name,
+  x <- data.table::melt(x, id.var = source_name,
             value.name = 'share',
             variable.factor = FALSE,
             variable.name = target_name)
   x <- x[share > 0]
-  setorderv(x, cols = get(paste0(sort_by, '_name')))
+  data.table::setorderv(x, cols = get(paste0(sort_by, '_name')))
   return(x)
 }
 
--- tests/testthat/test-reshaping.R
+++ tests/testthat/test-reshaping.R
@@ -1,6 +1,48 @@
 # load data
-CT_bin <- system.file('CT_bin_example', package = 'mapR')
+bin_mat_wide <- readRDS(system.file('testdata', 'CT_bin_example.RData', package = 'mapR'))
+num_mat_wide <- readRDS(system.file('testdata', 'CT_num_example.RData', package = 'mapR'))
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+bin_dt_wide <- as.data.table(bin_mat_wide, keep.rownames = 'UNFCCC_code')
+num_dt_wide <- as.data.table(num_mat_wide, keep.rownames = 'UNFCCC_code')
+
+
+test_that("reshaping binary CT works", {
+  expect_equal(
+    CT_to_wide(CT_to_long(bin_mat_wide), return = 'matrix'),
+    bin_mat_wide
+    )
+
+  expect_equal(
+    CT_to_wide(CT_to_long(bin_dt_wide, target_name = 'NACE_code'),
+               return = 'data.table'),
+    bin_dt_wide
+  )
 })
+
+
+test_that("reshaping numeric CT works", {
+
+  expect_equal(
+    CT_to_wide(CT_to_long(num_mat_wide), return = 'matrix'),
+    num_mat_wide
+  )
+
+  expect_equal(
+    CT_to_wide(CT_to_long(num_dt_wide, target_name = 'NACE_code'),
+               return = 'data.table'),
+    num_dt_wide
+  )
+})
+
+
+num_long1 <- CT_to_long(num_dt_wide)
+num_long2 <- copy(num_long1)
+num_long2[, c('a','b') := list(runif(.N), sample(LETTERS, .N, TRUE))]
+
+test_that('reshaping from long to wide works also for more than 3 columns CTs', {
+  expect_equal(
+    CT_to_wide(num_long1),
+    CT_to_wide(num_long2)
+  )
+})
+
